c5ebab2c302f7f24cb908fc497738a88fb40a3f3
Nikki Petersen <piknik@me.com>
2020-08-22 10:56:23 -0600

    add some basic note layout

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
 
 <style lang="scss">
 #app {
+  //font-family: 'Amatic SC', cursive;
+  //font-family: 'Architects Daughter', cursive;
+  //font-family: 'Caveat', cursive;
+  //font-family: 'Cedarville Cursive', cursive;
+  //font-family: 'Mrs Saint Delafield', cursive;
+  //font-family: 'Over the Rainbow', cursive;
+  //font-family: 'Reenie Beanie', cursive;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Quotes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div id="card" v-for="quote in quoteList" :key="quote.text">
+      <div class="card-body">
+          <p>{{ quote.text }} </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quotes',
+  props: ['quoteList']
+}
+</script>
+
+<style scoped>
+#card {
+  background-color: lightskyblue;
+  width: 300px;
+  height: auto;
+  border: 1px solid grey;
+}
+
+.card-body {
+  font-family: 'Architects Daughter', cursive;
+  font-size: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
+
+</style>
